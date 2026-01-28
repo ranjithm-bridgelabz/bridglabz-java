9abaa822cbe53e2f933c00be0d6f4a3ee40a934e
ranjithm-bridgelabz <ranjith.muthukrishnan@bridgelabz.com>
2026-01-28 15:10:29 +0530

    Day 15 DataStructure Hashtable Uc1 To Uc4

---
+++ src/com/java/HashtableandBst/MyMapNode.java
@@ -0,0 +1,12 @@
+package com.java.HashtableandBst;
+public class MyMapNode<K, V> {
+    K key;
+    V value;
+    MyMapNode<K, V> next;
+
+    public MyMapNode(K key, V value) {
+        this.key = key;
+        this.value = value;
+        this.next = null;
+    }
+}
