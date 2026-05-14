8e3c1f3275094c267618176ca14999c63eeddfce
ranjithm-bridgelabz <ranjith.muthukrishnan@bridgelabz.com>
2026-05-14 12:21:47 +0530

    Day 30 Invoice Generator

---
+++ src/com/java/Day30/Ride.java
@@ -0,0 +1,13 @@
+package Day30;
+
+public class Ride {
+
+    double distance;
+    int time;
+
+    public Ride(double distance, int time) {
+
+        this.distance = distance;
+        this.time = time;
+    }
+}
